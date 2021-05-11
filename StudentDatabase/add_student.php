625f5b64dc1f030c9ebc5453cc0a621a2a25129d
RITIK GAUTAM <65151717+ritikgautam@users.noreply.github.com>
2021-05-11 17:42:35 +0530

    Delete StudentDatabase directory

--- StudentDatabase/add_student.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include("connection.php");
-    $query = "INSERT INTO `student` ( `ROLL_NO`, `NAME`, `FATHER_NAME`, `CLASS`, `MOBILE_NO`, `email`, `password`, `REMARK`) VALUES ('$_POST[roll_no]', '$_POST[name]', '$_POST[father]', '$_POST[class]', '$_POST[mobile]', '$_POST[email]', '$_POST[password]', '$_POST[remark]')";
-    $data = mysqli_query($conn,$query);
-    ?>
-<script type="text/javascript">
-        alert("Details Added Succesfully");
-        window.location.href="admin_dashboard.php";
- </script>
